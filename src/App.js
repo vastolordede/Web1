92a9cc19d140de9554a81e356be074d0322635ba
vastolordede <vastolordede@gmail.com>
2024-10-01 18:31:26 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -14,39 +14,40 @@ function App() {
 
 
         <div class="container-fluid">
-       <ul class="nav navbar-nav">
-        <li class ="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" role="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-labelledby="dropdownMenuButton">General</a>
-            <div class="dropdown-menu" id="dropdownMenuButton" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="https://darksouls3.wiki.fextralife.com/General+Information">General</a>
-            </div> 
-        </li>
+          <ul class="nav navbar-nav">
+            <li class ="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" role="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-labelledby="dropdownMenuButton">General</a>
+                <div class="dropdown-menu" id="dropdownMenuButton" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="https://darksouls3.wiki.fextralife.com/General+Information">General</a>
+                </div> 
+            </li>
 
 
-        <li class ="nav-item">
-          <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/Character+Information">Character</a>
-        </li>
-        <li class ="nav-item">
-          <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/Equipment+&+Magic">Equipment & Magic</a>
-        </li>
-        <li class ="nav-item">
-          <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/World+Information">World</a>
-        </li>
-       </ul>
+            <li class ="nav-item">
+              <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/Character+Information">Character</a>
+            </li>
+            <li class ="nav-item">
+              <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/Equipment+&+Magic">Equipment & Magic</a>
+            </li>
+            <li class ="nav-item">
+              <a class="nav-link active" href="https://darksouls3.wiki.fextralife.com/World+Information">World</a>
+            </li>
+          </ul>
        </div>
       </nav>
-      <div className="text-center">
-      <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+Wiki">DS3</a><br></br>
-      <img src={img} alt="Cinque Terre" className="rounded-corners" width="560" height="300"/>
-      </div>
-      <div className="text-left mt-3">
+        <div className="text-center">
+            <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+Wiki">DS3</a><br></br>
+            <img src={img} alt="Cinque Terre" className="rounded-corners" width="560" height="300"/>
+        </div>
+        <div className="text-left mt-3">
             <p>Dark Souls III is an action role-playing video game developed by From Software and published by Bandai Namco for PlayStation 4, Xbox One, and Microsoft Windows. The third entry in the "Dark Souls" series, Dark Souls III was released in Japan in March 24th 2016, and worldwide in April 12th 2016.
             See <a href="https://darksouls3.wiki.fextralife.com/New+Player+Help">New Player Help</a> for an overview of game basics, and <a href="https://darksouls3.wiki.fextralife.com/Common+Souls+Terms">Common Souls Terms</a> for an evolving glossary of the community lexicon. </p>
-      <div className="text-center">
-      <a href=" https://darksouls3.wiki.fextralife.com/Ashes+of+Ariandel"> Ashes of Ariandel </a><br></br>
-      <img src={img1} alt="Cinque Terre" className="rounded-corners"  width="560" height="300" />
-      </div>
-      <div className="text-left mt-3">
+        </div>
+        <div className="text-center">
+            <a href=" https://darksouls3.wiki.fextralife.com/Ashes+of+Ariandel"> Ashes of Ariandel </a><br></br>
+            <img src={img1} alt="Cinque Terre" className="rounded-corners"  width="560" height="300" />
+        </div>
+        <div className="text-left mt-3">
             <p>Ashes of Ariandel is the first of two <a href="https://darksouls3.wiki.fextralife.com/DLC">DLCs</a> in <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+Wiki">Dark Souls 3</a>. It was released on October 25th. To access this DLC, interact with Slave Knight Gael at the Cleansing Chapel Bonfire in the Cathedral of the Deep. This expansion takes place in a new area called the Painted World of Ariandel, featuring new challenges, enemies, and lore for players to uncover.
             <br></br>On April 21st, <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+The+Fire+Fades+Edition">DARK SOULS III: The Fire Fades GOTY Edition</a> was released, which includes the base game as well as both expansions, Ashes of Ariandel and <a href="https://darksouls3.wiki.fextralife.com/The+Ringed+City">The Ringed City</a>.
             <br></br>Ashes of Ariandel launched October 25th on PS4, Xbox One and PC for $14.99, or is part of the Season Pass.
@@ -54,16 +55,15 @@ function App() {
             <br></br>Due to an error of releasing the DLC  for testing, it was available for download, to owners of the Season Pass, during the patch on October 20th/21st between 1:00 UTC and 7:00 UTC. The DLC was quickly removed from the store to await its initial release date</p>
         </div>   
         <div className="text-center">
-      <a href="https://darksouls3.wiki.fextralife.com/The+Ringed+City">The Ringed City</a><br></br>
-      <img src={img2} alt="Cinque Terre" className="rounded-corners"  width="560" height="300" />
-      </div>
-      <div className="text-left mt-3">
+            <a href="https://darksouls3.wiki.fextralife.com/The+Ringed+City">The Ringed City</a><br></br>
+            <img src={img2} alt="Cinque Terre" className="rounded-corners"  width="560" height="300" />
+        </div>
+        <div className="text-left mt-3">
             <p class="text-left">The Ringed City DLC is the second and final <a href="https://darksouls3.wiki.fextralife.com/DLC">DLC</a> for <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+Wiki">Dark Souls 3</a>. It was announced on January 23rd, 2017 by Bandai Namco and released on March 28th, 2017. It is recommended by the developers for players that have cleared <a href="https://darksouls3.wiki.fextralife.com/Lothric+Castle">Lothric Castle</a>. This can be accessed from the Kiln of the First Flame, where you'll need to go to a bonfire near a ramp that leads to the Soul of Cinder Arena. You can also access this from Sister Friede's arena located in the Painted World of Ariandel. These entry points allow players to venture into new areas and face unique challenges within the DLC. 
               On April 21st, <a href="https://darksouls3.wiki.fextralife.com/Dark+Souls+3+The+Fire+Fades+Edition">Dark Souls 3: The Fire Fades GOTY Edition</a> released which includes the base game as well as both expansions, Ashes of Ariandel and The Ringed City.
             <br></br>The Ringed City launched March 28th on PS4, Xbox One and PC for 14.99 or is part of the Season Pass.
             <br></br>Screenshots pre-launch: Article here, and here
             <br></br>Dark Souls 3 The Ringed City Review! 9.5/10</p>
-      </div>
         </div>
       </div>
     //toi day la noi dung
